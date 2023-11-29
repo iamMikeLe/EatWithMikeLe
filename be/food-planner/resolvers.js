8b1b5784026ede9f1c73d875f3512ee8735a0344
Mike Le / ubuntu <iam@mike-le.info>
2023-11-29 15:54:34 +0100

    dummy data

--- be/food-planner/resolvers.js
+++ be/food-planner/resolvers.js
@@ -1,5 +1,28 @@
+const dummyUser = {
+  userId: "123131",
+  firstName: "John",
+  lastName: "Doe",
+  email: "john@email.com",
+  password: "password",
+  avatar: "http://www.somelink.com/images/",
+  favorites: [],
+  learned: [],
+};
+
+const dummyMeal = {
+  mealId: "123441",
+  name: "spaghetti",
+  description: "spaghetti yummy",
+  tags: ["pasta", "sugar"],
+  author: "John",
+  authorId: "123441",
+};
+
+
+
 export const resolvers = {
   Query: {
-    greeting: () => 'Hello worldy!',
+    user: () => dummyUser,
+    meal: () => dummyMeal,
   },
 };
