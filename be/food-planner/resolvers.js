87f4f50d33ffb76096cb576f94f9ff3571bb7d4a
mike <iam@mike-le.info>
2023-12-01 15:18:21 +0100

    query user melas

--- be/food-planner/resolvers.js
+++ be/food-planner/resolvers.js
@@ -15,10 +15,23 @@ export const resolvers = {
       // db fetch function by user id here needed
       return dummyUser;
     },
+
     users: () => dummyUsers,
+
     meal: (_root, { id }) => dummyMeal,
+
     meals: () => dummyMeals,
+
     place: (_root, { id }) => dummyPlace,
+
     places: () => dummyPlaces,
+  },
+
+  User: {
+    meals: (user) => {
+      console.log(user.id);
+      // db fetch meals by user id function needed here
+      return dummyMeals;
+    },
   },
 };
