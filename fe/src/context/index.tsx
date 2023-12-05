84dcdfe9388fbdf9315155bf57ded851bfade1cd
Mike_Le <iam@mike-le.info>
2023-12-05 23:45:38 +0100

    cleanup fe

--- fe/src/context/index.tsx
+++ fe/src/context/index.tsx
@@ -1,25 +1,3 @@
-/* eslint-disable no-unused-vars */
-
-/**
-=========================================================
-* Material Dashboard 2 PRO React TS - v1.0.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-2-pro-react-ts
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-/**
-  This file is used for controlling the global states of the components,
-  you can customize the states for the different components here.
-*/
-
 import { createContext, ReactNode, useContext, useMemo, useReducer } from "react";
 
 // The Material Dashboard 2 PRO React TSUI Dashboard PRO Material main context
