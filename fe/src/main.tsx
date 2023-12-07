44af9df852db068762acb9e52c0e15738a0e8db5
Mike_Le <iam@mike-le.info>
2023-12-07 01:10:56 +0100

    migrating to vite

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,19 +1,20 @@
-import App from "App";
-import { createRoot } from "react-dom/client";
+import App from 'App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
-
 // Material Dashboard 2 PRO React TS Context Provider
 import { MaterialUIControllerProvider } from "context";
-const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
     <MaterialUIControllerProvider>
       <Provider store={store}>
         <App />
       </Provider>
     </MaterialUIControllerProvider>
   </BrowserRouter>
-);
+  </React.StrictMode>,
+)
