da6b3bdbcfabc55c3d201b1e3d06a1e579deb90b
Mike_Le <iam@mike-le.info>
2023-12-09 16:16:33 +0100

    code cleanup

--- fe/src/API/MealsAPI.ts
+++ fe/src/API/MealsAPI.ts
@@ -1,10 +1,10 @@
-import { dummyMeals } from "pages/meals/Dummy";
+import { dummyMeals } from "API/Dummy";
 import { MealData } from "pages/meals/mealSlice";
 
 // A mock function to mimic making an async request for data
 export function fetchMeals(): Promise<{ data: MealData[] }> {
   return new Promise<{ data: MealData[] }>((resolve) =>
-    setTimeout(() => resolve({ data: dummyMeals }), 3000)
+    setTimeout(() => resolve({ data: dummyMeals }), 1000)
   );
 }
 
