6a3148c4efecf79d8c20a6f4f76e314047819ca4
Mike_Le <iam@mike-le.info>
2023-12-12 00:34:48 +0100

    form cleanup + locale

--- fe/src/pages/addMeal/AddMealForm.tsx
+++ fe/src/pages/addMeal/AddMealForm.tsx
@@ -1,4 +1,3 @@
-import Autocomplete from "@mui/material/Autocomplete";
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
 import { useTranslation } from "react-i18next";
@@ -8,11 +7,6 @@ import Box from "components/Box";
 import Button from "components/Button";
 import FormField from "components/FormField";
 import Typography from "components/Typography";
-
-const selectData = {
-  gender: ["Male", "Female"],
-  skills: ["react", "vue", "angular", "svelte", "javascript"],
-};
 
 function AddMealForm(): JSX.Element {
   const { t } = useTranslation();
@@ -24,51 +18,15 @@ function AddMealForm(): JSX.Element {
       <Box component="form" pb={3} px={3}>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6}>
-            <FormField label="First Name" placeholder="Alec" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Autocomplete
-              defaultValue="Male"
-              options={selectData.gender}
-              renderInput={(params) => (
-                <FormField
-                  {...params}
-                  label="I'm"
-                  InputLabelProps={{ shrink: true }}
-                />
-              )}
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
             <FormField
-              label="Email"
-              placeholder="example@email.com"
-              inputProps={{ type: "email" }}
+              label={t("TITLE")}
+              placeholder={t("TITLE_PLACEHOLDER")}
             />
           </Grid>
           <Grid item xs={12} sm={6}>
             <FormField
-              label="confirmation email"
-              placeholder="example@email.com"
-              inputProps={{ type: "email" }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Autocomplete
-              multiple
-              defaultValue={["react", "angular"]}
-              options={selectData.skills}
-              renderInput={(params) => (
-                <FormField {...params} InputLabelProps={{ shrink: true }} />
-              )}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <FormField
-              label="Phone Number"
-              placeholder="+40 735 631 620"
-              inputProps={{ type: "number" }}
+              label={t("DESCRIPTION")}
+              placeholder={t("DESCRIPTION_PLACEHOLDER")}
             />
           </Grid>
           <Grid item xs={12} sm={12} mt={2}>
