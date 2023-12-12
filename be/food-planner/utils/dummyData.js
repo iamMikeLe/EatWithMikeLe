bd065c40a20c9463540e0da1ff7d9bfc2db24883
Mike_Le <iam@mike-le.info>
2023-12-12 20:21:28 +0100

    adjust dummy data

--- be/food-planner/utils/dummyData.js
+++ be/food-planner/utils/dummyData.js
@@ -29,9 +29,8 @@ export const dummyUsers = [
     avatar: "http://www.somelink.com/images/",
     favorites: [],
     learned: [],
-  }
+  },
 ];
-
 
 export const dummyMeal = {
   mealId: "123441",
@@ -44,23 +43,105 @@ export const dummyMeal = {
 
 export const dummyMeals = [
   {
-    mealId: "123441",
-    name: "spaghetti",
-    description: "spaghetti yummy",
-    tags: ["pasta", "sugar"],
-    author: "John",
-    authorId: "123441",
+    mealId: "1456456",
+    imageUrl: "homeDecor2",
+    title: "Sandwitch",
+    description: "yummy sandwitch",
+    tag: ["tag1", "tag2", "tag3"],
+    author: "Mike John",
+    authorId: "123456789",
+    createdAt: "2021-10-10",
+    modifiedAt: "2021-10-10",
   },
   {
-    mealId: "12fafa3441",
-    name: "spfasfasfafasaghetti",
-    description: "spaghetti yummy",
-    tags: ["pasta", "sugar"],
-    author: "John",
-    authorId: "123441",
-  }
+    mealId: "1456457",
+    imageUrl: "https://picsum.photos/320/190",
+    title: "Pizza",
+    description: "Delicious pizza",
+    tag: ["tag4", "tag5", "tag6"],
+    author: "John Doe",
+    authorId: "123456780",
+    createdAt: "2021-10-11",
+    modifiedAt: "2021-10-11",
+  },
+  {
+    mealId: "1456458",
+    imageUrl: "https://picsum.photos/320/190",
+    title: "Burger",
+    description: "Tasty burger",
+    tag: ["tag7", "tag8", "tag9"],
+    author: "Jane Doe",
+    authorId: "123456781",
+    createdAt: "2021-10-12",
+    modifiedAt: "2021-10-12",
+  },
+  {
+    mealId: "1456459",
+    imageUrl: "https://picsum.photos/320/190",
+    title: "Pasta",
+    description: "Creamy pasta",
+    tag: ["tag10", "tag11", "tag12"],
+    author: "Emma Watson",
+    authorId: "123456782",
+    createdAt: "2021-10-13",
+    modifiedAt: "2021-10-13",
+  },
+  {
+    mealId: "1456460",
+    imageUrl: "https://picsum.photos/320/190",
+    title: "Salad",
+    description: "Healthy salad",
+    tag: ["tag13", "tag14", "tag15"],
+    author: "Tom Cruise",
+    authorId: "123456783",
+    createdAt: "2021-10-14",
+    modifiedAt: "2021-10-14",
+  },
+  {
+    mealId: "1456461",
+    imageUrl: "https://picsum.photos/320/190",
+    title: "Steak",
+    description: "Juicy steak",
+    tag: ["tag16", "tag17", "tag18"],
+    author: "Robert Downey Jr.",
+    authorId: "123456784",
+    createdAt: "2021-10-15",
+    modifiedAt: "2021-10-15",
+  },
+  {
+    mealId: "1456462",
+    imageUrl: "https://picsum.photos/320/190",
+    title: "Fish",
+    description: "Grilled fish",
+    tag: ["tag19", "tag20", "tag21"],
+    author: "Chris Hemsworth",
+    authorId: "123456785",
+    createdAt: "2021-10-16",
+    modifiedAt: "2021-10-16",
+  },
+  {
+    mealId: "1456463",
+    imageUrl: "https://picsum.photos/320/190",
+    title: "Chicken",
+    description: "Roasted chicken",
+    tag: ["tag22", "tag23", "tag24"],
+    author: "Scarlett Johansson",
+    authorId: "123456786",
+    createdAt: "2021-10-17",
+    modifiedAt: "2021-10-17",
+  },
+  {
+    mealId: "1456464",
+    imageUrl: "https://picsum.photos/320/190",
+    title: "Rice",
+    description: "Fried rice",
+    tag: ["tag25", "tag26", "tag27"],
+    author: "Chris Evans",
+    authorId: "123456787",
+    createdAt: "2021-10-18",
+    modifiedAt: "2021-10-18",
+  },
 ];
-
 
 export const dummyPlace = {
   placeId: "123441",
@@ -74,7 +155,6 @@ export const dummyPlace = {
     latitude: "1223",
   },
 };
-
 
 export const dummyPlaces = [
   {
@@ -100,6 +180,5 @@ export const dummyPlaces = [
       longitude: "123",
       latitude: "1223",
     },
-  }
+  },
 ];
-
