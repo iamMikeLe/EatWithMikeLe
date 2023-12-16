10d90b559a8d47f88b6810c40e815a62e4e859d1
Mike_Le <iam@mike-le.info>
2023-12-16 16:39:09 +0100

    installing mongoose dotenv bodyparser

--- be/food-planner/server.js
+++ be/food-planner/server.js
@@ -1,21 +1,29 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
-import cors from 'cors';
-import express from 'express';
-import { readFile } from 'node:fs/promises';
-import { resolvers } from './resolvers.js';
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import express from "express";
+import { readFile } from "node:fs/promises";
+import HttpError from "./models/http-error.js";
+import { resolvers } from "./resolvers.js";
 
 const PORT = 9000;
 
 const app = express();
-app.use(cors(), express.json());
+app.use(cors(), express.json(), bodyParser.json());
 
-
-const typeDefs = await readFile('./schema.graphql', 'utf8');
+const typeDefs = await readFile("./schema.graphql", "utf8");
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 await apolloServer.start();
-app.use('/graphql', apolloMiddleware(apolloServer));
+app.use("/graphql", apolloMiddleware(apolloServer));
+
+// error handling for unknown routes
+app.use((_req, _res, _next) => {
+  const error = new HttpError("could not find this route.", 404);
+  throw error;
+});
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
