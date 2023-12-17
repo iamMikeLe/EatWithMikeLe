05d0c5786f90e7515cc94c013df8d1f75b84ff69
Mike_Le <iam@mike-le.info>
2023-12-17 14:39:29 +0100

    commented function to create meal

--- be/food-planner/controllers/meal-controller.js
+++ be/food-planner/controllers/meal-controller.js
@@ -3,6 +3,22 @@ import Meal from "../models/meal.js";
 
 export const getAllMeals = async () => {
   let meals;
+  /*   const createdMeal = new Meal({
+    title: "Gnochi",
+    description: "potato gnochi with tomato sauce",
+    imageUrl: "https://picsum.photos/320/190",
+    tags: ["potato", "tomato", "vegetarian"],
+    author: "Mike",
+    authorId: "123456456",
+  });
+
+  try {
+    await createdMeal.save();
+  } catch (err) {
+    const error = new HttpError("constants.CREATE_MEAL_FAILED", 500);
+    console.log("error", error);
+  }
+ */
   try {
     meals = await Meal.find();
   } catch (err) {
