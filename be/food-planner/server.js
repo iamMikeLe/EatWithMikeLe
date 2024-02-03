63e787867b2b4d995d97f4887f78ccc98110e16e
Mike_Le <iam@mike-le.info>
2024-02-03 17:25:43 +0100

    await bugfix

--- be/food-planner/server.js
+++ be/food-planner/server.js
@@ -17,46 +17,52 @@ import {
   UNKNOWN_ERROR,
 } from "./utils/constants.js";
 
-const app = express();
-app.use(cors(), express.json(), bodyParser.json(), authMiddleware);
+async function startServer() {
+  const app = express();
+  app.use(cors(), express.json(), bodyParser.json(), authMiddleware);
 
-// login route
-app.post("/login", handleLogin);
+  // login route
+  app.post("/login", handleLogin);
 
-const port = process.env.APP_PORT;
-const typeDefs = await readFile("./schema.graphql", "utf8");
+  const port = process.env.APP_PORT;
+  const typeDefs = await readFile("./schema.graphql", "utf8");
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
-await apolloServer.start();
-app.use(
-  "/graphql",
-  apolloMiddleware(apolloServer, { context: ({ req }) => getContext({ req }) })
-);
+  const apolloServer = new ApolloServer({ typeDefs, resolvers });
+  await apolloServer.start();
+  app.use(
+    "/graphql",
+    apolloMiddleware(apolloServer, {
+      context: ({ req }) => getContext({ req }),
+    })
+  );
 
-// error handling for unknown routes
-app.use(() => {
-  const error = new HttpError(INVALID_ROUTE, 404);
-  throw error;
-});
+  // error handling for unknown routes
+  app.use(() => {
+    const error = new HttpError(INVALID_ROUTE, 404);
+    throw error;
+  });
 
-// error handling middleware
-app.use((error, _req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.status || 500);
-  res.json({ message: error.message || UNKNOWN_ERROR });
-});
+  // error handling middleware
+  app.use((error, _req, res, next) => {
+    if (res.headerSent) {
+      return next(error);
+    }
+    res.status(error.status || 500);
+    res.json({ message: error.message || UNKNOWN_ERROR });
+  });
 
-console.log("Connecting to database...", process.env.MONGO_DB);
-mongoose
-  .connect(process.env.MONGO_DB)
-  .then(() => {
-    console.log("connected successfully");
-    app.listen({ port }, () => {
-      console.log(`Server running on port ${port}`);
-      console.log(`GraphQL endpoint: http://localhost:${port}/graphql`);
-    });
-  })
-  // eslint-disable-next-line no-console
-  .catch((err) => console.log(DATABASE_CONNECTION_FAILED, err));
+  console.log("Connecting to database...", process.env.MONGO_DB);
+  mongoose
+    .connect(process.env.MONGO_DB)
+    .then(() => {
+      console.log("connected successfully");
+      app.listen({ port }, () => {
+        console.log(`Server running on port ${port}`);
+        console.log(`GraphQL endpoint: http://localhost:${port}/graphql`);
+      });
+    })
+    // eslint-disable-next-line no-console
+    .catch((err) => console.log(DATABASE_CONNECTION_FAILED, err));
+}
+
+startServer().catch((err) => console.error(err));
